28b77f666f16696ff757ab9b057d171502662674
Vilker Rainer <142421864+vilkerrainer@users.noreply.github.com>
2025-04-30 14:44:45 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "gestao-clientes-back.vercel.app";
 let clienteEditandoId = null;
 let compraEditandoId = null;
 let modalCompraClienteId = null;
